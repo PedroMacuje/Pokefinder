7b716b2f63160b05a725ab159e6d711da28912d1
Pedro Macuje <pedro.macuje@usabit.com.br>
2026-04-29 15:10:29 -0300

    feat: create home page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
+import Home from "./pages/home";
+
 export default function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold">Pokedex Pro</h1>
-    </div>
-  );
+  return <Home />;
 }
