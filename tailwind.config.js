5e0fc4d6e07c02e91f12349287b1067ce1bde1f0
Pedro Macuje <pedro.macuje@usabit.com.br>
2026-04-30 17:06:01 -0300

    feat: add fade-up animation for staggered card entrance

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import { transform } from 'typescript';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,8 +7,24 @@ export default {
   "./src/**/*.{js,ts,jsx,tsx}",
 ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes:{
+        fadeUp:{
+          "0%":{
+            opacity: "0",
+            transform: "translateY(20px) scale (0.95)",
+          },
+          "100%":{
+            opacity: "1",
+            transform: "translateY(0) scale(1)"
+          }
+        }
+      },
+      animation: {
+        "fade-up": "fadeUp 0.5s ease forwads"
+      }
+    }
   },
   plugins: [],
-}
+};
 
