b6319a720220672f1549a4e86dfe4e83f67c778e
Pedro Macuje <pedro.macuje@usabit.com.br>
2026-04-30 17:33:22 -0300

    fix: fix tailwind values

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,28 +3,27 @@ import { transform } from 'typescript';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-  "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",
-],
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
-      keyframes:{
-        fadeUp:{
-          "0%":{
+      keyframes: {
+        fadeUp: {
+          "0%": {
             opacity: "0",
-            transform: "translateY(20px) scale (0.95)",
+            transform: "translateY(20px) scale(0.95)",
           },
-          "100%":{
+          "100%": {
             opacity: "1",
-            transform: "translateY(0) scale(1)"
-          }
-        }
+            transform: "translateY(0) scale(1)",
+          },
+        },
       },
       animation: {
-        "fade-up": "fadeUp 0.5s ease forwads"
-      }
-    }
+        "fade-up": "fadeUp 0.5s ease forwards",
+      },
+    },
   },
   plugins: [],
 };
-
