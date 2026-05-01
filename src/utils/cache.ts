a2d48d7c9774a695f8453a028b1a735589081341
Pedro Macuje <pedro.macuje@usabit.com.br>
2026-05-01 15:29:53 -0300

    feat: add localStorage functions to save pokemon list

---
+++ src/utils/cache.ts
@@ -0,0 +1,19 @@
+import { type Pokemon } from "../types/pokemon";
+
+const STORAGE_KEY = "pokemonCache";
+
+export function saveToCache(pokemons: Pokemon[]) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(pokemons));
+}
+
+export function loadFromCache(): Pokemon[] {
+  const data = localStorage.getItem(STORAGE_KEY);
+
+  if (!data) return [];
+
+  try {
+    return JSON.parse(data);
+  } catch {
+    return [];
+  }
+}
