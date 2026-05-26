de544856938d72787b36672071054d3d5b92ee8c
Pedro Macuje <pedro.macuje@usabit.com.br>
2026-05-26 17:26:22 -0300

    fix(searchBar): fix properties names

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -13,11 +13,11 @@ export default function SearchBar({ onChange, value }: SearchBarProps) {
           className="mr-2 h-5 w-5 stroke-slate-400"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="2"
+          strokeWidth="2"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
           ></path>
         </svg>
